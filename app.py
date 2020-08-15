688820db88da2f333950f91b513496a2dbb2878c
RuchiChopraStudent <69416691+RuchiChopraStudent@users.noreply.github.com>
2020-08-15 11:23:19 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,39 +1,32 @@
 # Importing the Libraries
 import logging
 import os
+# import pickle
 import sys
 import urllib
+
 import flask
 from flask import Flask, request, render_template
 from flask_cors import CORS
-from keras.datasets import imdb
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Flatten
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
 from textblob import TextBlob
 
-# load the dataset but only keep the top n words, zero the rest
-top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
-
-max_words = 500
-X_train = sequence.pad_sequences(X_train, maxlen=max_words)
-X_test = sequence.pad_sequences(X_test, maxlen=max_words)
-def perform_sentiment_analysis_cnn(text):
-    # create the model
-    model = Sequential()
-    model.add(Embedding(top_words, 32, input_length=max_words))
-    model.add(Flatten())
-    model.add(Dense(250, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
+def get_text_sentiment(text):
+    '''
+    Utility function to classify sentiment of passed text
+    using textblob's sentiment method
+    '''
+    # create TextBlob object of passed app text
     text = text.replace("+", " ")
     analysis = TextBlob(text)
+
+    # set sentiment
     if not analysis.sentiment.polarity < 0:
         return 'POSITIVE'
+    # elif analysis.sentiment.polarity == 0:
+    #     return 'MIXED'
     else:
         return 'NEGATIVE'
+
 
 # Loading Flask and assigning the model variable
 app = Flask(__name__)
@@ -42,6 +35,10 @@ app = flask.Flask(__name__, template_folder='templates')
 
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
+
+# with open('pickle_model_senti.pkl', 'rb') as handle:
+#     model = pickle.load(handle)
+
 
 @app.route('/')
 def main():
@@ -53,11 +50,32 @@ def main():
 def predict():
     url = request.get_data(as_text=True)[5:]
     text = urllib.parse.unquote(url)
-    outcome = perform_sentiment_analysis_cnn(text)
+    outcome = get_text_sentiment(text)
+
+    # LOAD SAVED PICKLE MODEL AND TEST IT WITH SOME RANDOM TEXT
+    # from sklearn.feature_extraction.text import CountVectorizer
+    # from keras.preprocessing import sequence
+    #
+    # # mdl = pickle.load(open('pickle_model_senti.pkl', 'rb'))
+    # cv = CountVectorizer()
+    # text = [text]
+    # text_vect = cv.fit_transform(text).toarray()
+    #
+    # max_words = 500
+    # text_vect = sequence.pad_sequences(text_vect, maxlen=max_words)
+    # pred = model.predict(text_vect)
+    # outcome = "Positive"
+    # if pred == 0:
+    #     outcome = "Positive"
+    # else:
+    #     outcome = "Negative"
+
+    # Passing the news article to the model and returing whether it is Fake or Real
+    # pred = model.predict([news])
+    # return render_template('main.html', prediction_text='The news is "{}"'.format(outcome))
     return render_template('main.html', prediction_text='It is the ' + outcome + ' sentiment')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(port=port, debug=True, use_reloader=False)
-
